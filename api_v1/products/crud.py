6c80ef582befc305a62bcafbeefea648e93d751a
eztoen <ikucheren0510@gmail.com>
2025-10-13 21:58:05 +0600

    Create get_product

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -1,17 +1,11 @@
-from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models import Products
 
-from fastapi import Path
-from products.schemas import ProductSchema
+async def get_product(session: AsyncSession) -> list[Products]:
+    stmt = select(Products).order_by(Products.id)
+    result: Result = await session.execute(stmt)
+    products = result.scalars().all()
+    return list(products)
 
-def create_product(product: ProductSchema) -> dict:
-    product = product.model_dump()
-    return {
-        'success': True,
-        'product': product,
-    }
-    
-def get_product_by_id(id: Annotated[int, Path(ge=1, lt=1_000_000)]) -> dict:
-    return {
-        'id': id,
-        'product': 'product'
-    }
--- core/models/product.py
+++ core/models/product.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Mapped
 from .base import Base
 
 class Products(Base):
+    id: Mapped[int]
     name: Mapped[str]
     descriptions: Mapped[str]
     price: Mapped[int]
