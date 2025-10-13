249ee6c8fa6d62dd8917b851ffbdf9fbc6b4e0ee
eztoen <ikucheren0510@gmail.com>
2025-10-13 20:29:07 +0600

    delete tablename

--- core/models/product.py
+++ core/models/product.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import Mapped
 from .base import Base
 
 class Products(Base):
-    __tablename__ = 'products'
-    
     name: Mapped[str]
     descriptions: Mapped[str]
     price: Mapped[int]
