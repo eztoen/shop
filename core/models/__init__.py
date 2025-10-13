909fd4392cefaf8e6ef67db9ce3802e8d5214ca7
eztoen <ikucheren0510@gmail.com>
2025-10-13 19:44:35 +0600

    Create lifespan for db

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     'Base',
+    'async_engine',
+    'async_session_factory',
     'Products'
 )
 
-from .base import Base
+from .base import Base, async_engine, async_session_factory
 from .product import Products
--- main.py
+++ main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from contextlib import asynccontextmanager
+
+from core.models import Base, async_engine, async_session_factory
 from products.views import router as products_router
 from users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+   
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(products_router)
 app.include_router(users_router)
 
