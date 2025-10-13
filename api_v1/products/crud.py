8b99af94656aee6c702b2a7085acb461831392ee
eztoen <ikucheren0510@gmail.com>
2025-10-13 22:06:47 +0600

    Create create_product

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -3,11 +3,20 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Products
 
+from .schemas import ProductSchema
+
 async def get_product(session: AsyncSession) -> list[Products]:
     stmt = select(Products).order_by(Products.id)
     result: Result = await session.execute(stmt)
     products = result.scalars().all()
     return list(products)
 
-async def get_product_by_id(product_id: int, session: AsyncSession) -> Products | None:
+async def get_product_by_id(session: AsyncSession, product_id: int) -> Products | None:
     return await session.get(Products, product_id)
+
+async def create_product(session: AsyncSession, new_product: ProductSchema) -> Products:
+    product = Products(**new_product.model_dump())
+    session.add(product)
+    await session.commit()
+    await session.refresh(product)
+    return product
