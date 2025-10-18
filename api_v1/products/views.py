4e43fcda33ea2a4d046d6c39b5dd2898e52e9470
eztoen <ikucheren0510@gmail.com>
2025-10-18 22:50:10 +0600

    Add session

--- api_v1/products/views.py
+++ api_v1/products/views.py
@@ -1,16 +1,18 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.models.base import DatabaseHelper
 from . import crud
 from .schemas import ProductSchema, Product
 
 router = APIRouter(prefix='/products', tags=['Products'])
 
 @router.get('/', response_model=list[Product])
-async def get_products(session):
+async def get_products(session: AsyncSession = Depends(DatabaseHelper.session_dependency())):
     return await crud.get_products(session=session)
 
 @router.get('/{product_id}', response_model=Product)
-async def get_product_by_id(product_id: int, session):
+async def get_product_by_id(product_id: int, session: AsyncSession = Depends(DatabaseHelper.session_dependency())):
     product = await crud.get_product_by_id(session=session, product_id=product_id)
     if product is not None:
         return product
