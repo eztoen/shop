f883443b6aa1e4956c614708e32762685b5af8d3
eztoen <ikucheren0510@gmail.com>
2025-10-20 22:16:30 +0600

    Minor updates

--- products/views.py
+++ products/views.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Path, HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import DatabaseHelper
+from core.models import db_helper
 from . import crud
 from .schemas import ProductSchema, Product
 
 router = APIRouter(prefix='/products', tags=['Products'])
 
 @router.get('/', response_model=list[Product])
-async def get_products(session: AsyncSession = Depends(DatabaseHelper.session_dependency)):
+async def get_products(session: AsyncSession = Depends(db_helper.get_scoped_session)):
     return await crud.get_products(session=session)
 
 @router.get('/{product_id}', response_model=Product)
-async def get_product_by_id(product_id: int, session: AsyncSession = Depends(DatabaseHelper.session_dependency)):
+async def get_product_by_id(product_id: int, session: AsyncSession = Depends(db_helper.get_scoped_session)):
     product = await crud.get_product_by_id(session=session, product_id=product_id)
     if product is not None:
         return product
@@ -23,5 +23,5 @@ async def get_product_by_id(product_id: int, session: AsyncSession = Depends(Dat
     )
 
 @router.post('/', response_model=Product)
-async def create_product(session, new_product: ProductSchema):
-    return await crud,create_product(session=session, new_product=new_product)
+async def create_product(new_product: ProductSchema, session: AsyncSession = Depends(db_helper.get_scoped_session) ):
+    return await crud.create_product(session=session, new_product=new_product)
